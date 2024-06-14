a75e1a214d38d70dc7f8f5b7eb5eb824c22a3aef
nadiasembiring08 <136603312+nadiasembiring08@users.noreply.github.com>
2024-06-14 17:43:20 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,66 +1,178 @@
-<?php
-session_start();
-if(isset($_SESSION['username'])){
-    header('location:dashboard.php');
-}else{
-    ?>
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SIA | LOGIN</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-</head>
-
-<body class="bg-secondary">
-    <div class="container">
-        <div class="row">
-            <div class="col-md4 m-auto mt-5 shadow p-3 bg-white">
-                <form action="authentification.php" method="post">
-                    <h3 class="text-center">Login System</h3>
-                    <hr>
-                    <?php
-                    if(isset($_SESSION['pesan'])){
-                        ?>
-                        <div class="alert alert-danger"><?= $_SESSION['pesan'];?></div>
-                        <?php
-                    }
-                    ?>
-                    </hr>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">
-                            <i class="bi bi-person-fill"></i>
-                        </span>
-                        <input type="text" class="form-control" name="username" placeholder="username">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">
-                            <i class="bi bi-key-fill"></i>
-                        </span>
-                        <input type="password" class="form-control" name="password" placeholder="password">
-                    </div>
-                    <div class="mb-3 d-grid">
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </div>
-                </form>
-                <div class="text-center">
-                    <small>Aplikasi Sistem Informasi Akuntansi | </small>
+<div class="card mb-3">
+    <div class="card-body">
+        <form action="" method="post">
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <label for="invoice" class="form-label">Invoice</label>
+                    <input type="text" class="form-control" name="invoice">
+                </div>
+                <div class="col-md-4">
+                    <label for="tanggal" class="form-label">Tanggal</label>
+                    <input type="date" class="form-control" name="tanggal">
+                </div>
+                <div class="col-md-4">
+                    <label for="suplier" class="form-label">Suplier</label>
+                    <select name="suplier" class="form-select">
+                        <option value="1">PT Suplier Jaya</option>
+                        <option value="2">CV Maju Jaya</option>
+                    </select>
                 </div>
             </div>
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <label for="jumlah" class="form-label">Jumlah</label>
+                    <input type="number" class="form-control" name="jumlah">
+                </div>
+                <div class="col-md-4">
+                    <label for="harga" class="form-label">Harga</label>
+                    <div class="input-group">
+                        <span class="input-group-text">Rp.</span>
+                        <input type="number" class="form-control" name="harga">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <label for="total" class="form-label">Total</label>
+                    <div class="input-group">
+                        <span class="input-group-text">Rp.</span>
+                        <input type="number" class="form-control" name="total" disabled>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="keterangan" class="form-label">Keterangan</label>
+                    <textarea name="keterangan" class="form-control"></textarea>
+                </div>
+            </div>
+            <hr class="text-secondary">
+            <div class="text-end">
+                <button type="reset" class="btn btn-secondary">Reset</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header">
+        <h3>Data Pembelian</h3>    
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Invoice</th>
+                        <th>Tanggal</th>
+                        <th>Suplier</th>
+                        <th>Jumlah</th>
+                        <th>Harga</th>
+                        <th>Total</th>
+                        <th>Keterangan</th>
+                        <th><i class="bi bi-gear-fill"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>BL110324</td>
+                        <td>11/03/2024</td>
+                        <td>PT Suplier Jaya</td>
+                        <td>1</td>
+                        <td>Rp. 15.000,000,-</td>
+                        <td>Rp. 15.000.000,-</td>
+                        <td>Pembelian perlengkapan maintenance</td>
+                        <td>
+                            <a href="#editPembelian" class="text-decoration-none" data-bs-toggle="modal">
+                                <i class="bi bi-pencil-square text-success"></i>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                                <i class="bi bi-trash text-danger"></i>
+                             </a>
+                            </td>
+                            <!-- Modal -->
+                            <div class="modal fade" id="editPembelian" tabindex="-1" arialabelledby="exampleModalLabel" aria-hidden="true">
+                                <form action="" method="post">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Data Pembelian</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="row mb-3">\<div class="col-md-4">
+                                                    <label for="invoice" class="form-label">Invoice</label>
+                                                    <input type="text" class="form-control" name="invoice" value="BL110324" disabled>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <label for="tanggal" class="form-label">Tanggal</label>
+                                                    <input type="date" class="form-control" name="tanggal" value="2024-11-03">
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <label for="suplier" class="form-label">Suplier</label>
+                                                    <select name="suplier" class="form-select">
+                                                        <option value="1" selected>PT Suplier Jaya</option>
+                                                        <option value="2">CV Maju Jaya</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="col-md-4">
+                                                    <label for="jumlah" class="form-label">Jumlah</label>
+                                                    <input type="number" class="form-control" name="jumlah" value="1">
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <label for="harga" class="form-label">Harga</label>
+                                                    <div class="input-group">
+                                                        <span class="input-group-text">Rp.</span>
+                                                        <input type="number" class="form-control"
+                                                        name="harga" value="15000000">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <label for="total" class="form-label">Total</label>
+                                                    <div class="input-group">
+                                                        <span class="input-group-text">Rp.</span>
+                                                        <input type="number" class="form-control" name="total" value="15000000" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <label for="keterangan" class="form-label">Keterangan</label>
+                                                    <textarea name="keterangan" class="form-control">Pembelian perlengkapan maintenance</textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary">Simpan</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>BL100324</td>
+                        <td>10/03/2024</td>
+                        <td>CV Maju Jaya</td>
+                        <td>5</td>
+                        <td>Rp. 10.000.000,-</td>
+                        <td>Rp. 50.000.000,-</td>
+                        <td>Pengadaan 5 unit laptop ASUS</td>
+                        <td>
+                            <a href="" class="text-decoration-none">
+                                <i class="bi bi-pencil-square text-success"></i>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                                <i class="bi bi-trash text-danger"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
-    crossorigin="anonymous"></script>
-</body>
-</html>
-
-<?php
-}
-session_destroy();
-?>
+</div>
